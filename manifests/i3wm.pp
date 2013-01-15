71dce5f9a76d18db7d95d7119ffc92e302ad707c
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-01-15 15:48:04 -0500

    using existing i3wm found in the ubuntu repo

--- manifests/i3wm.pp
+++ manifests/i3wm.pp
@@ -3,39 +3,12 @@ class i3wm(
   $user = $workstation::config::user
 ) inherits workstation::config {
 
-  include 'apt'
-
-  apt::source { "i3wm":
-    location          => "http://debian.sur5r.net/i3",
-    release           => $lsbdistcodename,
-    repos             => "universe",
-    include_src       => false,
-    before            => Anchor['i3wm::repo'],
-  }
-
-  anchor { 'i3wm::repo': }
-
-  exec { "install-sur5r-keyring":
-    command => "/usr/bin/apt-get --allow-unauthenticated install sur5r-keyring",
-    require => Anchor['i3wm::repo'],
-    unless => "dpkg --get-selections | grep -E sur5r-keyring",
-    path => "/usr/bin:/bin",
-  }
-
-  exec {"apt-get-update":
-    command => "/usr/bin/apt-get update",
-    require => Exec["install-sur5r-keyring"],
-    subscribe => Exec["install-sur5r-keyring"],
-    refreshonly => true,
-  }
-
   package { 'i3':
-    ensure  => latest,
-    require => Exec["apt-get-update"],
+    ensure  => installed,
   }
 
   package { 'suckless-tools':
-    ensure => latest,
+    ensure => installed,
   }
 
   file { "$home/.i3":
@@ -46,6 +19,5 @@ class i3wm(
     source => "puppet:///modules/workstation/i3",
     recurse => true,
   }
-
 
 }
