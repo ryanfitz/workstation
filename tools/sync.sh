d0f2bd1fa3862b7ea31d58e30404ed2e64e13195
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-01-15 12:55:08 -0500

    adding script to sync up submodules with latest master commits

---
+++ tools/sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git pull origin master
+# git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
