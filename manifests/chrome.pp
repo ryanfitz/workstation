4bccf74aedcfdf3f0bc1b7ef9cfa99ce9e10d25d
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-01-15 15:19:44 -0500

    adding in google chrome repo key

--- manifests/chrome.pp
+++ manifests/chrome.pp
@@ -6,6 +6,7 @@ class chrome() {
     location          => "http://dl.google.com/linux/chrome/deb",
     release           => "stable",
     repos             => "main",
+    key               => "7FAC5991",
     key_source        => "https://dl-ssl.google.com/linux/linux_signing_key.pub",
     include_src       => false,
     before            => Anchor['chrome::repo'],
