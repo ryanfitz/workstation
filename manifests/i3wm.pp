2f33d26d83deb682502fe5e9887ca7cca4a628d9
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-01-15 18:17:05 -0500

    using i3wm provided repo to install i3

--- manifests/i3wm.pp
+++ manifests/i3wm.pp
@@ -3,8 +3,23 @@ class i3wm(
   $user = $workstation::config::user
 ) inherits workstation::config {
 
+  include 'apt'
+
+  apt::source { "i3wm":
+    location          => "http://debian.sur5r.net/i3/",
+    release           => $lsbdistcodename,
+    repos             => "universe",
+    key               => "CD693E9E",
+    key_source        => "puppet:///modules/workstation/sur5r-keyring.gpg",
+    include_src       => false,
+    before            => Anchor['i3wm::repo'],
+  }
+
+  anchor { 'i3wm::repo': }
+
   package { 'i3':
-    ensure  => installed,
+    ensure  => latest,
+    require => Anchor['i3wm::repo']
   }
 
   package { 'suckless-tools':
